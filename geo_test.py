456ec0d2816e610e9e6b7d866211e911bc7b3683
alturkaa <akram.alturk@gmail.com>
2015-09-21 09:52:52 -0400

    geoprocessing of sociology abstracts text

---
+++ geo_test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python2.7
+
+import numpy as np
+import pandas as pd
+#import matplotlib.pyplot as plt
+#plt.style.use('ggplot')
+import json
+import requests
+
+headers = {'Content-Type': 'application/json'}
+
+loc = r'/usr/akram/soc.csv'
+articles = pd.read_csv(loc)
+
+#def geocode(text):
+#    article_text = {'text': text}
+#    data = json.dumps(article_text)
+#    return data
+
+#articles['ab_text'] = articles['AB'].apply(geocode)
+
+def geocode(text):
+    article_text = {'text': text}
+    data = json.dumps(article_text)
+    out = requests.post('http://localhost:5000/country', data=data, headers=headers)
+    return out.text
+
+articles['AB_ISO'] = articles['AB'].apply(geocode)
+
+articles['AB_ISO'].head()
+
